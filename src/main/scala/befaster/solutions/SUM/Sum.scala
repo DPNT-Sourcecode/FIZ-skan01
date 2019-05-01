60e3e95cb1cb9a7ddf32930da7d749d2869c8ab4
sbx_user1051 <sbx_user1051@169.254.9.97>
2019-05-01 14:16:51 +0000

    Wed May 01 14:16:51 UTC 2019

--- src/main/scala/befaster/solutions/SUM/Sum.scala
+++ src/main/scala/befaster/solutions/SUM/Sum.scala
@@ -4,12 +4,9 @@ import befaster.runner.SolutionNotImplementedException
 
 object Sum {
 
-  def main(args: Array[String]): Unit = {
-    val a = sum(10, 20)
-    println(a)
-  }
   def sum(x: Int, y: Int): Int = {
     x + y
   }
 }
+
 
