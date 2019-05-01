a8f749597464f6c1d4e3719d3cff534eb7a93cf7
sbx_user1051 <sbx_user1051@169.254.9.97>
2019-05-01 14:34:12 +0000

    Wed May 01 14:34:12 UTC 2019

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -4,17 +4,38 @@ import befaster.runner.SolutionNotImplementedException
 
 object FizzBuzz {
 
+  def main(args: String[Array]): Unit = {
+    println(fizzBuzz(13))
+    println(fizzBuzz(55))
+    println(fizzBuzz(30))
+    println(fizzBuzz(35))
+    println(fizzBuzz(8))
+  }
+
+
+
   def fizzBuzz(number: Int): String = {
 
-    if((number % 3 == 0) && (number % 5 == 0))
+    var fizzFlag = false
+    var buzzFlag = false
+
+    if((number % 3 == 0) || (number.toString.contains("3")))
+      fizzFlag = true
+
+    if((number % 5 == 0) || (number.toString.contains("5")))
+      buzzFlag = true
+
+    if(fizzFlag && buzzFlag)
       "fizz buzz"
-    else if(number % 3 == 0)
+    else if(fizzFlag)
       "fizz"
-    else if(number % 5 == 0)
+    else if(buzzFlag)
       "buzz"
     else
       number.toString
+
   }
 }
+
 
 
